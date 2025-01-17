68a292f98e418c74cf6819f8f4bdcfca028ee55c
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    RM6785: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.RM6785
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
