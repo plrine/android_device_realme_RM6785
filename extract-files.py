a599d2d8993d0f136a781a5264bc240fc08a3ba3
claxten10 <claxten10@gmail.com>
2025-11-20 22:54:35 +0000

    RM6785: Switch to dummy libaedv
    Change-Id: I0f87e8536103cd59b41a2d8ee322c861a352e31d
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -20,6 +20,7 @@ from extract_utils.main import (
 namespace_imports = [
     'device/realme/RM6785',
 	'hardware/mediatek',
+    'hardware/mediatek/libaedv',
 	'hardware/mediatek/libmtkperf_client',
 ]
 
